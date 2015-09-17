f0d58a92dcacd206d3b26c05096ebe0ae44045d5
Snow811 <snowfox811@gmail.com>
2015-09-17 05:15:02 -0500

    Fixed #34 added fake leave command.

---
+++ src/main/java/com/builtbroken/vee/commands/CommandFLeave.java
@@ -0,0 +1,25 @@
+package com.builtbroken.vee.commands;
+
+import com.builtbroken.jlib.lang.TextColor;
+import com.builtbroken.mc.prefab.commands.AbstractCommand;
+import net.minecraft.command.ICommandSender;
+
+/**
+ * Created by snowf on 9/17/2015.
+ */
+public class CommandFLeave extends AbstractCommand {
+    public CommandFLeave() {
+        super("fleave");
+    }
+
+    @Override
+    public boolean handleConsoleCommand(ICommandSender sender, String[] args) {
+
+        addChatToAllPlayers(TextColor.YELLOW.getColorString()  +combine(args) + " left the game");
+        return true;
+    }
+        }
+
+
+
+
